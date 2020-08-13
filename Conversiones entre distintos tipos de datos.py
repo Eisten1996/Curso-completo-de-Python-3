53209016fe8a7efec8ff63d2aae29173baabeb0b
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-12 20:20:33 -0500

    Conversiones entre distintos tipos de datos

---
+++ Conversiones entre distintos tipos de datos.py
@@ -0,0 +1,18 @@
+# de numero a cadena
+numero = 5
+cadena = str(numero)
+print(type(cadena))
+
+# de cadena a numero entero
+cadena = "10"
+numero2 = int(cadena)
+print(type(numero2))
+suma = numero + numero2
+print(suma)
+
+# de cadena a numero decimal
+cadena = "10.6"
+numero2 = float(cadena)
+print(type(numero2))
+suma = numero + numero2
+print(suma)
