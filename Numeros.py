e20671de244b74951edbe9846ce6366e0300067b
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-12 20:07:04 -0500

    Números

---
+++ Numeros.py
@@ -0,0 +1,22 @@
+# numero1 es un numero
+numero1 = 5
+print(numero1)
+
+# Comprobamos que es un numero
+print(type(numero1))
+
+# Numero decimal
+numeroDecimal = 3.5
+print(numeroDecimal)
+print(type(numeroDecimal))
+
+# suma de numeros
+numero2 = 12
+suma = numero1 + numero2
+print(suma)
+print(type(suma))
+
+numero3 = 20.1
+suma = numero2 +numero3
+print(suma)
+print(type(suma))
