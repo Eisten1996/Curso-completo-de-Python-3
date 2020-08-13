78ec633b13d3d1ff93526ad82568a8e70abd4f23
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-12 19:44:09 -0500

    Variables

---
+++ Variables.py
@@ -0,0 +1,31 @@
+# Variable numero
+numero = 5
+print(numero)
+
+# Variable cadena
+cadena = "Hola mundo"
+print(cadena)
+
+# Sensible a mayuscula y minusculas
+Numero = 10
+numero = 20
+print(numero)
+print(Numero)
+
+# Concatenacion
+cadena2 = " Cruel"
+cadenaNueva = cadena + cadena2
+print(cadenaNueva)
+
+# Suma de numero
+numero1 = 10
+numero2 = 15
+suma = numero1+numero2
+print(suma)
+
+# Tipo de datos
+numero3 = 15
+print(type(numero3))
+
+cadena = "hola"
+print(type(cadena))
