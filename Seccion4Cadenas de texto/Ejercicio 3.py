ce27cdd10f7df5b639a42de3b8d822ce468fa6c4
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-22 12:36:56 -0500

    Ejercicio 3

---
+++ Seccion4Cadenas de texto/Ejercicio 3.py
@@ -0,0 +1,9 @@
+# Crear una variable "numero1" que contiene el valor de 5
+# Crear una variable "numero2" que contiene el valor de 8
+# Crear una variable "suma" que contiene la suma de los numero anteriores
+# Imprimir por pantalla el resulado, utilizando print
+
+numero1 = 5
+numero2 = 8
+suma = numero1+numero2
+print(f"El resultado de sumar {numero1} mas {numero2} es {suma}")
