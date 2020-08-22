7743b141a625d9edca85e4805406ec3b8d786a26
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-22 15:46:38 -0500

    Operadores aritméticos

---
+++ Sección 5-Operadores/Operadores aritméticos.py
@@ -0,0 +1,25 @@
+# Operadores aritmetico (+, -, *, /, *, **, //)
+
+numero1 = 110
+numero2 = 5
+
+suma = numero1 + numero2
+print(suma)
+
+resta = numero1 - numero2
+print(resta)
+
+multiplicacion = numero1 * numero2
+print(multiplicacion)
+
+division = numero1 / numero2 / 5
+print(division)
+
+resto = numero1 / numero2 % 5
+print(resto)
+
+cociente = numero1 // 3
+print(cociente)
+
+exponente = numero2**5
+print(exponente)
