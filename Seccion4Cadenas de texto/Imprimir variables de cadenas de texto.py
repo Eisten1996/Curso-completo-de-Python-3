0e5c70a514e808595055c7417b6f27198b52ef1d
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-22 10:33:43 -0500

    Imprimir variables de cadenas de texto

---
+++ Seccion4Cadenas de texto/Imprimir variables de cadenas de texto.py
@@ -0,0 +1,13 @@
+nombre = "Eisten"
+print("Buenos dias " + nombre)
+
+edad = 24
+print("Buenos dias {}, feliz {} cumpleaños".format(nombre, edad))
+
+resultado = 10 / 3
+print(resultado)
+print("El resultado es {r}".format(r=resultado))
+print("El resultado es {r:1.3f}".format(r=resultado))
+
+# f-strings
+print(f"Buenos dias {nombre}, feliz {edad} cumpleaños")
