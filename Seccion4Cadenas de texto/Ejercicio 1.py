896ea72bd206db4b0ded105c5cc23e36946a5ffc
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-22 12:04:13 -0500

    Ejercicio 1

---
+++ Seccion4Cadenas de texto/Ejercicio 1.py
@@ -0,0 +1,6 @@
+# Crear una variable "cadena" que contiene el texto "Esto es un texto de ejemplo"
+# Segun la disposicion de cada letra en la cadena, calcular que valores (x,y) hay que poner para selccionar solo la 
+# palabra "texto"
+
+cadena = "Esto es un texto de ejemplo"
+print(cadena[11:16])
